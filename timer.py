0f62ddec076e9c13492b74b4c4948d3b45f76d7a
mkscripps <mark.kelly@scrippsnetworks.com>
2013-06-28 14:21:10 -0400

    Adding elapsed time to timer function.

--- timer.py
+++ timer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 """
-Define a Timer context manager, allowing to measure the
-wall time of the code block it contains.
+A timer as a context manager.
+
+Wraps around a time generator. A custom generator can be passed
+to the constructor. The default timer is timeit.default_timer.
+
+This measures wall clock time, not CPU time!
+On Unix systems, it corresponds to time.time.
+On Windows systems, it corresponds to time.clock.
 
 Example:
 >>> with Timer() as timer:
@@ -26,14 +32,19 @@ from timeit import default_timer
 class Timer(object):
     """ A timer as a context manager. """
 
-    def __init__(self):
+    def __init__(self, timer=default_timer):
         self.timer = default_timer
         # measures wall clock time, not CPU time!
         # On Unix systems, it corresponds to time.time
         # On Windows systems, it corresponds to time.clock
 
+    def __call__(self):
+        """Return the current time"""
+        return self.timer()
+
     def __enter__(self):
         self.start = self.timer() # measure start time
+        self.end = None
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
@@ -41,3 +52,16 @@ class Timer(object):
         self.elapsed_s = self.end - self.start # elapsed time, in seconds
         self.elapsed_ms = self.elapsed_s * 1000  # elapsed time, in milliseconds
 
+    def elapsed(self):
+        """Return elapsed time in seconds"""
+        if self.end is None:
+            return self.timer() - self.start
+        else:
+            return self.end - self.start
+
+    def elapsed_ms(self):
+        """Return elapsed time in milliseconds"""
+        if self.end is None:
+            return (self.timer() - self.start) * 1000
+        else:
+            return (self.end - self.start) * 1000
